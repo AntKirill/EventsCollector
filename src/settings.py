81d7b2146cfcb2242d7c79cf338eb02e49da44cd
kant97 <kirant9797@gmail.com>
2018-12-15 15:11:22 +0300

    fixed compilation and moved out log from sources

--- src/settings.py
+++ src/settings.py
@@ -1,12 +1,13 @@
+import os
 from pathlib import Path
 
-ROOT_DIRECTORY = Path()
+ROOT_DIRECTORY = Path(os.path.abspath(__file__)).parent.parent
 
 LOG_FILENAME = 'events_collector.log'
 
 DEBUG = False
 
-try:
-    from .local_settings import *
-except ImportError:
-    pass
+# try:
+#     from .local_settings import *
+# except ImportError:
+#     pass
