c356e243d47a6997a138907aefef34528d17c6ed
kant97 <kirant9797@gmail.com>
2018-12-16 22:24:11 +0300

    add tests for google_calendar_manager

--- test/managers_tests.py
+++ test/managers_tests.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from unittest import TestCase
 from unittest.mock import patch
 
@@ -43,3 +44,93 @@ class TestGoogleCalendarManager(TestCase):
         os.removedirs("google_calendar")
         self.assertEqual(str, 'hello')
         self.assertEqual(self.x, 0)
+
+    @patch('src.google_calendar.google_calendar_client.GoogleCalendarClient')
+    def test_get_n_upcoming_events(self, MockClient):
+        client = MockClient()
+
+        client.get_upcoming_events.return_value = []
+        manager = google_calendar_manager.GoogleCalendarManager(client)
+        self.assertEqual(manager.get_n_upcoming_events(10), None)
+
+        client.get_upcoming_events.return_value = {"abc": "cba"}
+        manager = google_calendar_manager.GoogleCalendarManager(client)
+        self.assertEqual(manager.get_n_upcoming_events(1), {"abc": "cba"})
+
+    @patch('src.google_calendar.google_calendar_client.GoogleCalendarClient')
+    def test_get_all_day_events(self, MockClient):
+        client = MockClient()
+
+        client.get_todays_allday_events.return_value = None
+        manager = google_calendar_manager.GoogleCalendarManager(client)
+        self.assertEqual(manager.get_all_day_events("2018-09-06"), [])
+
+        client.get_todays_allday_events.return_value = [
+            {"start": {"dateTime": "hello"}, "stop": {"dateTime": "goodbay"}}]
+        manager = google_calendar_manager.GoogleCalendarManager(client)
+        self.assertEqual(manager.get_all_day_events("2018-10-13"), [])
+
+        events_list1 = [
+            {"start": {"1": "2"}}, {"start": {"start": "start"}}, {"start": {"2": "3"}, "stop": "hello"}
+        ]
+        client.get_todays_allday_events.return_value = events_list1
+        manager = google_calendar_manager.GoogleCalendarManager(client)
+        self.assertEqual(manager.get_all_day_events("2018-01-01"), events_list1)
+
+        events_list2 = [
+            {"start": {"1": "2"}}, {"start": {"start": "start"}},
+            {"start": {"dateTime": "goodbay"}, "end": {"stop": "finish"}}, {"start": {"2": "3"}, "stop": "hello"}
+        ]
+        client.get_todays_allday_events.return_value = events_list2
+        manager = google_calendar_manager.GoogleCalendarManager(client)
+        self.assertEqual(manager.get_all_day_events("2018-02-10"), events_list1)
+
+    @patch('src.google_calendar.google_calendar_client.GoogleCalendarClient')
+    def test_get_todays_all_day_events(self, MockClient):
+        client = MockClient()
+
+        client.get_todays_allday_events.return_value = None
+        manager = google_calendar_manager.GoogleCalendarManager(client)
+        self.assertEqual(manager.get_todays_all_day_events(), [])
+
+    @patch('src.google_calendar.google_calendar_client.GoogleCalendarClient')
+    def test_post_event(self, MockClient):
+        client = MockClient()
+
+        client.post_event.side_effect = self.__gc_event_paramets_validator__
+        manager = google_calendar_manager.GoogleCalendarManager(client)
+        manager.post_event("hello_world", "2018-09-10")
+        manager.post_event("hello_world", "2018-12-31")
+        manager.post_event("hello_world", "2018-01-01", "10:00:00")
+        manager.post_event("hello_world", "2018-11-30", "22:59:59")
+
+    def __gc_event_paramets_validator__(self, event):
+        start = event.get("start")
+        self.assertIsNotNone(start)
+        end = event.get('end')
+        self.assertIsNotNone(end)
+        start_date_time = start.get("dateTime")
+        if start_date_time is None:
+            date_format = "%Y-%m-%d"
+            start_date = start.get('date')
+            self.assertIsNotNone(start_date)
+            end_date = end.get('date')
+            self.assertIsNotNone(end_date)
+            try:
+                start_date_iso = datetime.strptime(start_date, date_format)
+                end_date_iso = datetime.strptime(end_date, date_format)
+            except ValueError:
+                self.fail("strptime raised ValueError unexpectedly!")
+            delta = end_date_iso - start_date_iso
+            self.assertEqual(delta.days, 1)
+        else:
+            date_time_format = "%Y-%m-%dT%H:%M:%S"
+            end_date_time = end.get('dateTime')
+            self.assertIsNotNone(end_date_time)
+            try:
+                start_date_time_iso = datetime.strptime(start_date_time, date_time_format)
+                end_date_time_iso = datetime.strptime(end_date_time, date_time_format)
+            except:
+                self.fail("strptime raised ValueError unexpectedly!")
+            delta = end_date_time_iso - start_date_time_iso
+            self.assertEqual(delta.seconds, 3600)
