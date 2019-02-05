1f9b76e65280a1fe3ba207f8abff6d0b7c9e90ca
kant97 <kirant9797@gmail.com>
2019-02-05 11:59:05 +0300

    add appindicator

---
+++ src/appindicator.py
@@ -0,0 +1,36 @@
+import os
+import signal
+
+from client import client
+
+APPINDICATOR_ID = 'events_collector_indicator_id'
+
+
+def indicator_main():
+    import gi
+
+    gi.require_version('Gtk', '3.0')
+    from gi.repository import Gtk as gtk
+
+    gi.require_version('AppIndicator3', '0.1')
+    from gi.repository import AppIndicator3 as appindicator
+
+    def build_menu():
+        menu = gtk.Menu()
+        item_quit = gtk.MenuItem('Quit')
+        item_quit.connect('activate', quit)
+        menu.append(item_quit)
+        menu.show_all()
+        return menu
+
+    def quit(source):
+        client.send_request(['-q', 'server_shutdown'])
+        gtk.main_quit()
+
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+    indicator = appindicator.Indicator.new(APPINDICATOR_ID, os.path.abspath('../icon/ec_logo.png'),
+                                           appindicator.IndicatorCategory.SYSTEM_SERVICES)
+    indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
+    indicator.set_menu(build_menu())
+    gtk.main()
