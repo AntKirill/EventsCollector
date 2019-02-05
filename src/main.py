4078606260a62df7a03be681f0d7cc89e722cc34
kant97 <kirant9797@gmail.com>
2019-02-05 12:00:14 +0300

    add server process and gui process, implement server api

--- src/main.py
+++ src/main.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 import logging
+import shlex
 import signal
 import socketserver
 import sys
 import time
 import urllib.request
+from multiprocessing import Process
 
 import humanfriendly
 
+import server_api as api
 import settings
 import state_tracker
 from EventsCollector import EventsCollector
+from appindicator import indicator_main
 
 
 def wait_for_internet_connection():
@@ -25,7 +29,7 @@ def wait_for_internet_connection():
             pass
 
 
-def pull_events():
+def gc_to_trello():
     wait_for_internet_connection()
     events_collector_instance = EventsCollector()
     events = events_collector_instance.get_today_allday_events_from_google_calendar()
@@ -37,6 +41,10 @@ def pull_events():
         logging.info('Posting to trello.com')
         events_collector_instance.post_to_trello(cur_state, "My To Do Board", "Incoming", new_events)
         state_tracker.update_state(cur_state, new_events)
+
+
+def trello_to_gc():
+    events_collector_instance = EventsCollector()
     events_collector_instance.shift_deadlines_from_trello_to_google_calendar("My To Do Board")
 
 
@@ -60,15 +68,35 @@ def init_logger(log_filename, max_size='1Mb'):
 class MyTCPHandler(socketserver.StreamRequestHandler):
     def handle(self):
         self.data = self.rfile.readline().strip()
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
-        pull_events()
+        # print("{} wrote:".format(self.client_address[0]))
+        query = shlex.split(self.data.decode("utf-8"))
+        parsed_query = api.parse_arguments(query)
+        q = parsed_query.query
+        logging.info('Have new query: {0}'.format(q))
+        if q == 'synch':
+            if settings.DEBUG:
+                print(q)
+                return
+            gc_to_trello()
+            trello_to_gc()
+        elif q == 'trello_to_gc':
+            if settings.DEBUG:
+                print(q)
+                return
+            trello_to_gc()
+        elif q == 'gc_to_trello':
+            if settings.DEBUG:
+                print(q)
+                return
+            gc_to_trello()
+        elif q == 'server_shutdown':
+            self.server.need_shutdown = True
 
 
 def run_ec_server():
     HOST, PORT = "localhost", 9999
 
-    # Create the server, binding to localhost on port 9999
+    # Create the server, binding to host HOST on port PORT
     server = socketserver.TCPServer((HOST, PORT), MyTCPHandler)
 
     def signal_handler(sig, frame):
@@ -77,13 +105,21 @@ def run_ec_server():
         sys.exit(0)
 
     signal.signal(signal.SIGINT, signal_handler)
+    server.need_shutdown = False
 
-    server.serve_forever()
+    while not server.need_shutdown:
+        server.handle_request()
 
 
 def main():
     init_logger(settings.LOG_FILENAME)
-    run_ec_server()
+    p = Process(target=run_ec_server)
+    q = Process(target=indicator_main)
+    p.start()
+    q.start()
+    p.join()
+    q.join()
+    # run_ec_server()
 
 
 if __name__ == '__main__':
